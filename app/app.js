67716fac27da7c930d800a39a1388737bafd0304
Edaena <edaena@edaena.com>
2014-10-06 23:09:49 -0700

    Basic div adding through ng-repeat

--- app/app.js
+++ app/app.js
@@ -1,7 +1,20 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('carcassonne', []);
+
+var myApp = angular.module('carcassonne',[]);
+
+myApp.controller('GreetingController', ['$scope', function($scope) {
+  $scope.greeting = 'Hola!';
+  var count = 2;
+  $scope.tiles = [1, 2];
+  
+  $scope.addTile = function() {
+  	$scope.tiles.push(count + 1);
+  	count++;
+  };
+  
+}]);
 
 /*config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
