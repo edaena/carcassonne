ae385ac54735d05209f8a9abb1811281ad547385
Edaena <edaena@edaena.com>
2014-10-07 21:05:16 -0700

    Grid with ng-repeat and click event to add column

--- app/app.js
+++ app/app.js
@@ -7,11 +7,19 @@ var myApp = angular.module('carcassonne',[]);
 myApp.controller('GreetingController', ['$scope', function($scope) {
   $scope.greeting = 'Hola!';
   var count = 2;
-  $scope.tiles = [1, 2];
+  $scope.rows = [];
+  $scope.cols = [];
   
-  $scope.addTile = function() {
-  	$scope.tiles.push(count + 1);
-  	count++;
+  for(var r=0; r<5; ++r) {
+  	 $scope.rows.push(r);
+  	 $scope.cols.push(r);
+  }
+  
+  $scope.makeMove = function(e, index, parentIndex) {
+  	console.log(parentIndex + " "+ index);
+	var elem = angular.element(e.srcElement);
+	elem.className='tile';
+	alert(elem.className);
   };
   
 }]);
