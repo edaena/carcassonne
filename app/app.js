2b89846243078e5c8b7063e7dd562074eee18c2d
Jeff Meyerson <meff.jeyerson@gmail.com>
2014-10-08 19:43:42 -0700

    can play carcassonne images

--- app/app.js
+++ app/app.js
@@ -6,7 +6,8 @@ var myApp = angular.module('carcassonne',[]);
 
 myApp.controller('GreetingController', ['$scope', function($scope) {
   $scope.tile = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 3);
-  var tiles_remaining = 10;
+  var tileNames = ['all-city_1', 'city-except-down_3', 'city-except-down_3', 'city-except-down_3'];
+  var tileIndex = 0;
   $scope.rows = [];
   $scope.cols = [];
   for(var r=0; r<15; ++r) {
@@ -15,12 +16,14 @@ myApp.controller('GreetingController', ['$scope', function($scope) {
   }
   
   $scope.makeMove = function(e, index, parentIndex) {
-	if(tiles_remaining-- > 0){  	
+	if(tileIndex < tileNames.length){
+		var tileName = tileNames[tileIndex++];
 		console.log(parentIndex + " "+ index);
 		var elem = angular.element(e.srcElement);
 		elem.addClass("tile");
 		elem.text($scope.tile);
-		$scope.tile = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 3);
+		var imagePath = "<img src='tile_images/" + tileName + ".png'>";
+		elem.html(imagePath);
 	}	
   };
   
